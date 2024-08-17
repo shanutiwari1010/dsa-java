c313c12b68fe8c4c59db1cf05adf6da953da073e
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-08-17 16:43:53 +0530

    isSorted array recursion

---
+++ recursion/array/IsSorted.java
@@ -0,0 +1,21 @@
+package recursion.array;
+
+public class IsSorted {
+    public static void main(String[] args) {
+        int arr[] = {1,2,9,4,5};
+        System.out.println( isSorted(arr,0));
+    }
+    public static boolean isSorted(int arr[], int idx){
+        if (arr == null || arr.length == 0) {
+            return true; // Consider an empty array as sorted
+        }
+        if(idx == arr.length-1){
+            return true;
+        }
+        if(arr[idx] > arr[idx+1]){
+            return false;
+        }
+
+        return isSorted(arr,idx+1);
+    }
+}
